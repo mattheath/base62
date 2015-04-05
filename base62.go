6b171ca36f9c1ae36c50e69945f5ed4762e305d6
Matt Heath <matt@mattheath.com>
2015-04-05 20:31:43 +0100

    Add encoding for big.Int

--- base62.go
+++ base62.go
@@ -3,6 +3,7 @@ package base62
 
 import (
 	"math"
+	"math/big"
 	"strings"
 )
 
@@ -48,4 +49,26 @@ func DecodeToInt64(s string) int64 {
 	}
 
 	return int64(n)
+}
+
+// EncodeBigInt return the base62 encoding of arbitrary precision integers
+func EncodeBigInt(n *big.Int) string {
+	var (
+		s    []byte   = make([]byte, 0)
+		rem  *big.Int = new(big.Int)
+		base *big.Int = new(big.Int)
+		zero *big.Int = new(big.Int)
+	)
+	base.SetInt64(62)
+	zero.SetInt64(0)
+
+	// Progressively divide by base, until we hit zero
+	// store remainder each time
+	// Prepend as an additional character is the higher power
+	for n.Cmp(zero) == 1 {
+		n, rem = n.DivMod(n, base, rem)
+		s = append([]byte{encodeStd[rem.Int64()]}, s...)
+	}
+
+	return string(s)
 }
--- base62_test.go
+++ base62_test.go
@@ -1,9 +1,11 @@
 package base62
 
 import (
+	"math/big"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 var testcases = []struct {
@@ -37,5 +39,39 @@ func TestDecodeToInt64(t *testing.T) {
 		v := DecodeToInt64(tc.encoded)
 		t.Logf("Decoded %s as %v", tc.encoded, v)
 		assert.Equal(t, tc.num, v)
+	}
+}
+
+var bigTestcases = []struct {
+	num     string
+	encoded string
+}{
+	{"1", "1"},
+	{"9", "9"},
+	{"10", "A"},
+	{"35", "Z"},
+	{"36", "a"},
+	{"61", "z"},
+	{"62", "10"},
+	{"99", "1b"},
+	{"3844", "100"},
+	{"3860", "10G"},
+	{"4815162342", "5Frvgk"},
+	{"9223372036854775807", "AzL8n0Y58m7"},
+}
+
+func TestEncodeBigInt(t *testing.T) {
+	for _, tc := range bigTestcases {
+		var (
+			n  *big.Int = new(big.Int)
+			ok bool
+		)
+
+		n, ok = n.SetString(tc.num, 10)
+		require.True(t, ok)
+
+		v := EncodeBigInt(n)
+		t.Logf("Encoded %v as %s", tc.num, v)
+		assert.Equal(t, tc.encoded, v)
 	}
 }
